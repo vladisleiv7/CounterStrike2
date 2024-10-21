feec55ac8b80e437a4fd5bfd5ea0df712976e85f
vladisleiv7 <dkflrf1274@gmail.com>
2024-10-21 07:50:54 +0300

    fix01

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let scrollInterval;
 
 function scrollUp() {
     if (window.pageYOffset > 0) {
-        window.scrollBy(0, -10);
+        window.scrollBy(0, -15);
     } else {
         clearInterval(scrollInterval); 
     }
@@ -32,3 +32,45 @@ scrollToTopBtn.addEventListener('mouseup', function() {
 scrollToTopBtn.addEventListener('mouseleave', function() {
     clearInterval(scrollInterval); 
 });
+
+
+   
+    const questions = document.querySelectorAll('.voprosi a'); 
+    questions.forEach(question => {
+        question.addEventListener('click', function(event) {
+            event.preventDefault(); 
+
+            const targetId = this.getAttribute('href'); 
+            const targetElement = document.querySelector(targetId); 
+
+            
+            targetElement.scrollIntoView({
+                behavior: 'smooth', 
+                block: 'start' 
+            });
+        });
+    });
+
+
+    
+    document.querySelectorAll('.voprosi a').forEach(item => {
+        item.addEventListener('click', function(event) {
+            event.preventDefault(); 
+    
+            
+            const answerId = this.getAttribute('href');
+    
+            
+            const answerElement = document.querySelector(answerId);
+    
+            
+            document.querySelectorAll('.highlight').forEach(element => {
+                element.classList.remove('highlight');
+            });
+    
+            
+            answerElement.classList.add('highlight');
+        });
+    });
+    
+    
